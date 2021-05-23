e1abc2f5d4e83c78d5af9d9aace31712cf2af7e1
Arthur Mauvezin <arthur.mauvezin@teolia.fr>
2021-05-23 23:00:36 +0200

    Init project

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Teolia - Get on your Apprenticeship !
+
+Ce projet est conçu comme un tp itératif te permettant de tester tes connaissances et d'apprendre ce qui te manque pour l'instant via des exemples simples.
+
+> ⚠️  **WARNING**: N'aie crainte, tu n'as pas besoin de tout faire avant de revenir vers nous avec tes questions et ou l'envie de présenter ton projet. D'ailleurs, on t'invite à rejoindre notre Discord pour poser tes questions ou montrer où tu en es dans ton projet: https://discord.gg/8KdQZZCFNK
+
+
---
+++ api/routes/dummy.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/student', function (req, res, next) {
+  res.send('Harry Potter !');
+});
+
+router.get('/students', function (req, res, next) {
+  res.json([
+    {
+      name: 'Harry Potter',
+      house: 'Gryffindor'
+    },
+    {
+      name: 'Hermione Granger',
+      house: 'Gryffindor'
+    },
+    {
+      name: 'Ron Weasley',
+      house: 'Gryffindor'
+    },
+    {
+      name: 'Neville Longbottom',
+      house: 'Gryffindor'
+    },
+    {
+      name: 'Luna Lovegood',
+      house: 'Ravenclaw'
+    }
+  ])
+});
+
+module.exports = router;
