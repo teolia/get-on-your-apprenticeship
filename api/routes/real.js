91cc3cf6627137023cb37299671ddd83b1b13979
Arthur Roux <arthur.roux@epita.fr>
2021-06-16 19:52:07 +0200

    init firestore before changing call backend

--- api/routes/real.js
+++ api/routes/real.js
@@ -2,6 +2,13 @@ var express = require('express');
 const axios = require('axios').default;
 var router = express.Router();
 
+const Firestore = require('@google-cloud/firestore');
+
+const db = new Firestore({
+projectId: 'harrypotter-api-316818',
+keyFilename: './key/harrypotter-api-316818-09c318c27f60.json',
+});
+
 const URL_POUDLARSTUDENTS_API = 'http://hp-api.herokuapp.com/api/characters';
 
 router.get('/', function (req, res) {
